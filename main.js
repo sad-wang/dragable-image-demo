c2f64ea8eb6e92ba03544b50bf5aa5af5ff14a11
sad wang <leown_wang@163.com>
2020-04-21 15:01:48 +0800

    Test

--- main.js
+++ main.js
@@ -9,6 +9,39 @@
             this.model = model
             this.lib = lib
             this.test()
+            this.test1()
+        },
+        test1() {
+            let stickers = this.lib.getElement('#stickers')
+            let star1 = this.lib.getElement('#star1')
+            let canvas = this.lib.getElement('.canvas')
+            let pointerX, pointerY
+            star1.onclick = e => {
+                e.preventDefault()
+                let newNode = star1.cloneNode(true)
+                newNode.style.position = 'absolute'
+                canvas.appendChild(newNode)
+                let state = false
+                newNode.onmousedown = e => {
+                    pointerX = e.clientX
+                    pointerY = e.clientY
+                    state = true
+                }
+                canvas.onmousemove = e => {
+                    e.preventDefault()
+                    if (state) {
+                        let dX =  e.clientX - pointerX
+                        let dY =  e.clientY - pointerY
+                        newNode.style.left = newNode.offsetLeft + dX + 'px'
+                        newNode.style.top = newNode.offsetTop + dY + 'px'
+                        pointerX = e.clientX
+                        pointerY = e.clientY
+                    }
+                }
+                canvas.onmouseup = e => {
+                    state = false
+                }
+            }
         },
         test() {
             let stickers = this.lib.getElement('#stickers')
@@ -18,6 +51,7 @@
             star.onclick = e => {
                 e.preventDefault()
                 let newNode = star.cloneNode(true)
+                newNode.style.position = 'absolute'
                 canvas.appendChild(newNode)
                 let state = false
                 newNode.onmousedown = e => {
